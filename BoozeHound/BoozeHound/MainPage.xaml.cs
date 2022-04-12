8670fa9541b0b2a0303445443f85875f841b1d1a
Connor <c.mccurrie@gmail.com>
2022-04-11 21:16:42 -0500

    Initial commit

---
+++ BoozeHound/BoozeHound/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BoozeHound
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private int legalAge = 0;
+        private string country = "";
+        private DateTime userBirthday;
+        void Handle_Clicked(object Sender, System.EventArgs e)
+        {
+            
+        }
+    }
+}
